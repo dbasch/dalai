4eb92c1a6c6e38be7fa9308ce063ad400a44df29
MacMini <MacMini@MacMinis-Mac-mini.local>
2011-02-18 17:56:20 -0800

    simple

--- dalai.rb
+++ dalai.rb
@@ -1,15 +1,5 @@
 require 'sinatra'
-require './markov'
 
 get '/' do
     "hello"
-"""
-
-    m = MarkovTool.new
-    w = m.words_from_file('dalai.txt')
-    m.markov_data_from_words( w )
-    # create a paragraph and display it
-    p = m.get_paragraph
-    p
-    """
 end
